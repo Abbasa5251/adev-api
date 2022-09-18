517ae494a83df4cf7d2cb9f53deaac6ab59274b3
Abbas Anandwala <abbasa5251@hotmail.com>
2022-09-18 17:37:59 +0530

    interim: celery email using redis backend

--- core/settings/development.py
+++ core/settings/development.py
@@ -22,7 +22,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = env.int("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
