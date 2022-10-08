6eb71bf62c3e7fbbf23617c04598da280946f9db
Abbasa5251 <abbasa5251@hotmail.com>
2022-10-08 15:34:14 +0530

    added more fields to about section

--- about/models.py
+++ about/models.py
@@ -1,3 +1,5 @@
+from phonenumber_field.modelfields import PhoneNumberField
+
 from common.models import TimeStampedUUIDModel
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -5,6 +7,12 @@ from django.utils.translation import gettext_lazy as _
 
 class About(TimeStampedUUIDModel):
     description = models.TextField(verbose_name=_("description"))
+    address = models.TextField(verbose_name=_("address"))
+    city = models.CharField(max_length=64, verbose_name=_("city"))
+    email = models.EmailField(verbose_name=_("email"))
+    phone_number = PhoneNumberField(
+        max_length=14, verbose_name=_("phone number"), null=True, blank=True
+    )
 
     class Meta:
         verbose_name = _("About")
