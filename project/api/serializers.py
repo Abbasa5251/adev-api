8ca0ae4083f9a81b9327aaaae0854c06c9c16829
Abbasa5251 <abbasa5251@hotmail.com>
2023-08-20 14:48:09 +0530

    fix: fixed the swagger API doc issues

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -1,4 +1,3 @@
-from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from project.models import Project, Tag
@@ -25,12 +24,10 @@ class ProjectSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ("id",)
 
-    @swagger_serializer_method(serializer_or_field=serializers.URLField)
     def get_image(self, obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.image_url)
 
-    @swagger_serializer_method(serializer_or_field=TagsListSerializerField)
     def get_tags(self, obj):
         _tags = obj.tags.all()
         return [tag.name for tag in _tags]
@@ -56,12 +53,10 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ("id",)
 
-    @swagger_serializer_method(serializer_or_field=serializers.URLField)
     def get_image(self, obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.image_url)
 
-    @swagger_serializer_method(serializer_or_field=TagsListSerializerField)
     def get_tags(self, obj):
         _tags = obj.tags.all()
         return [tag.name for tag in _tags]
